85816237550807c441a499dee11ae85ea0cd0db5
Jay Sueno <jay.sueno@gmail.com>
2020-09-18 21:54:31 -0700

    working on bar graph

--- Code/static/js/app.js
+++ Code/static/js/app.js
@@ -4,9 +4,10 @@
 // read in the json file using "d3.json()" and assign to a variable to be able to call later
 var dataFile = d3.json("../data/samples.json").then(function(data) 
     {console.log("json data file read", data)});
-d3.json("../data/samples.json").then(function(data){
-    console.log(data.names)
-})
+
+// d3.json("../data/samples.json").then(function(data){
+//     console.log(data.names)
+// })
 
 
 // optionChanged() is a function in the drop down dial box
@@ -14,9 +15,11 @@ d3.json("../data/samples.json").then(function(data){
 
 // create a listener for the dropdown menue and assign it a variable for future use
 var dropDown = d3.select("#selDataset");
+
 // create a variable to reference the names of the datasets
 // this create an array of the names values from the json object
-var idName = data.names;
+// var idName = data.names;
+
 // append the dropdown menu to include all the names of the dataset using D3
 // we will use the .append() function to the "<select></>" node
 // d3.json("../data/samples.json").then(function(data) {
@@ -41,16 +44,53 @@ d3.json("../data/samples.json").then(function(data) {
         // we map the data that's already merged and pull out each item directly 
         .attr("value", (i) => i)
         .text(i => i);
+    
+    // pull init values out for bar chart, bubble, and meta data
+    // use .reverse() to sort the values
+    var sample_values = data.samples[0].sample_values.slice(0,10);
+    var otu_ids = data.samples[0].otu_ids.slice(0,10);
+    var otu_labels = data.samples[0].otu_labels.slice(0,10);
+    console.log(sample_values, otu_ids, otu_labels);
+
+    var trace = {
+        x: sample_values,
+        y: otu_ids,
+        text: otu_labels,
+        // name: "otu values",
+        type: "bar",
+        orientation: "h"
+    };
+
+    var data = [trace];
+
+    var layout = {
+        title: "OTU Values",
+        xaxis: {
+            title: "this is a test"
+        },
+        yaxis: {
+            categoryorder: "total ascending"
+        },
+        margin: {
+            l: 50,
+            r: 50,
+            t: 50,
+            b: 50
+        }
+    };
+
+    Plotly.newPlot("bar", data, layout)
 });
 
 // create the bar chart with the selected dataset
 // initialize the dashboard with preloaded data
-function init() {
-    var trace = {
-        // we need to iterate through an array of objects within a list of objects
-        x: 
-    };
-}
+// function init() {
+//     var filteredID = 
+//     var trace = {
+//         // we need to iterate through an array of objects within a list of objects
+//         x: 
+//     };
+// }
 
 /**
  * Helper function to select stock data
