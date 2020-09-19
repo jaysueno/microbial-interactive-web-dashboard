87035e7fd627e45b0ccd391c640aafc29bb50e04
Jay Sueno <jay.sueno@gmail.com>
2020-09-18 17:14:57 -0700

    created listner to dropdown menu

--- Code/static/js/app.js
+++ Code/static/js/app.js
@@ -1,4 +1,16 @@
-var test = d3.json("../data/samples.json")
+// Plotly application to showcase my skills in creating engaging visualization and a web dashboard 
+// Written by Jay Sueno
 
-// .then(function(data) 
-    // {console.log(data)});
+// read in the json file using "d3.json()" and assign to a variable to be able to call later
+var dataFile = d3.json("../data/samples.json").then(function(data) 
+    {console.log("json data file read", data)});
+
+// optionChanged() is a function in the drop down dial box
+// id for listener ".selDataset"
+
+// create a listener for the dropdown menue and assign it a variable for future use
+var dropdown = d3.select(".selDataset");
+
+// append the dropdown menu to include all the names of the dataset using D3
+// we will use the .append() function to the "<select></>" node
+
