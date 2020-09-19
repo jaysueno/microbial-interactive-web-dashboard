89cad0f54f2dbd402df762e0aae31c1aff8bb2f5
Jay Sueno <jay.sueno@gmail.com>
2020-09-18 18:10:21 -0700

    populated the dropdown menu using .forEach()

--- Code/static/js/app.js
+++ Code/static/js/app.js
@@ -4,13 +4,27 @@
 // read in the json file using "d3.json()" and assign to a variable to be able to call later
 var dataFile = d3.json("../data/samples.json").then(function(data) 
     {console.log("json data file read", data)});
+d3.json("../data/samples.json").then(function(data){
+    console.log(data.names)
+})
+
 
 // optionChanged() is a function in the drop down dial box
 // id for listener ".selDataset"
 
 // create a listener for the dropdown menue and assign it a variable for future use
-var dropdown = d3.select(".selDataset");
+var dropDown = d3.select("#selDataset");
 
 // append the dropdown menu to include all the names of the dataset using D3
 // we will use the .append() function to the "<select></>" node
-
+d3.json("../data/samples.json").then(function(data) {
+    // create a variable to reference the names of the datasets
+    // this create an array of the names values from the json object
+    var idName = data.names;
+    idName.forEach(name => {
+        dropDown.append("option")
+        .attr("value", name)
+        .text(name);
+    console.log(idName);
+    })
+});
